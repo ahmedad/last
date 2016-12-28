702b4c928696b557f9b7366025247bf8abef91ec
ahmedad <ahmedad2077@gmail.com>
2016-12-29 02:34:23 +0300

    added routes again

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, controllers: { confirmations: 'confirmations' }
   resources :about
   get 'about' => 'about#index'
   resources :posts do
