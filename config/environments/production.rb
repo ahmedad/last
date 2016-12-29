c81272318d11d8fd4073ed812d874b034105f1b3
ahmedad <ahmedad2077@gmail.com>
2016-12-29 16:42:15 +0300

    styled and few changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { :host => "www.voteyangu.com" }
   config.action_mailer.delivery_method = :elastic_email
   config.action_mailer.elastic_email_settings = {
     api_key: '87eb9870-2cd9-4869-b42e-a79f22eb3877',
